caa3cd7ff0a932eff0807a2a90bab692c3a29b2b
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-17 05:15:52 +0530

    Created URLs for sign-up, log-in and compose

--- amplio/urls.py
+++ amplio/urls.py
@@ -6,4 +6,7 @@ app_name = 'amplio'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/$', views.search, name='search'),
+    url(r'^sign-up/$', views.sign_up, name='sign_up'),
+    url(r'^log-in/$', views.log_in, name='log_in'),
+    url(r'^compose/$', views.compose, name='compose'),
 ]
