d181e45837442f291644f3b065bf88556103f725
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-16 22:30:35 +0530

    Converted numeric strings to numbers and added FEEDBACK_TYPE_CHOICES, FEEDBACK_TO_CHOICES and FEEDBACK_CATEGORY_CHOICES

--- amplio/choices.py
+++ amplio/choices.py
@@ -1,6 +1,47 @@
 USER_POST_CHOICES = {
-    {'1': 'Student'},
-    {'2': 'IMG developer'},
-    {'3': 'IMG designer'},
-    {'4': 'IIT official'},
+    {1: 'Student'},
+    {2: 'IMG developer'},
+    {3: 'IMG designer'},
+    {4: 'IIT official'},
+}
+
+FEEDBACK_TYPE_CHOICES = {
+    {1: 'Suggestions for improving existing apps'},
+    {2: 'Bugs that need to be eliminated rapidly'},
+    {3: 'Glitches that need to be eliminated casually'},
+    {4: 'Ideas for building new apps'},
+}
+
+FEEDBACK_TO_CHOICES = {
+    {0: 'All concerned'},
+    {1: 'Developers only'},
+    {2: 'Designers only'},
+}
+
+FEEDBACK_CATEGORY_CHOICES = {
+    {0: 'No specific category or miscellaneous'},
+    {1: 'IIT Roorkee website'},
+    {2: 'IMG website'},
+    {3: 'New entrants'},
+    {4: 'Channel-i'},
+    {5: 'Notice board'},
+    {6: 'Lectures and tutorials'},
+    {7: 'Lost and found'},
+    {8: 'Buy and sell'},
+    {9: 'People search'},
+    {10: 'Jukebox'},
+    {11: 'Kriti'},
+    {12: 'Yaadein'},
+    {13: 'Projects portal'},
+    {14: 'Internship portal'},
+    {15: 'Placement portal'},
+    {16: 'Student home-page'},
+    {17: 'DC++ hub'},
+    {18: 'Bunk-o-meter Android app'},
+    {19: 'Notice board Android app'},
+    {20: 'Forum'},
+    {21: 'FAQ'},
+    {22: 'Media management system'},
+    {23: 'Forminator'},
+    {24: 'Status'},
 }
