44f47dd35c0e70efba121f1af16839c3b10c26bb
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-17 05:14:43 +0530

    Added views for search, sign-up, log-in and compose

--- amplio/views.py
+++ amplio/views.py
@@ -1,5 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'amplio/index.html')
+
+
+def search(request):
+    return HttpResponse("Work in progress")
+
+
+def sign_up(request):
+    return HttpResponse("Work in progress")
+
+
+def log_in(request):
+    return HttpResponse("Work in progress")
+
+
+def compose(request):
+    return HttpResponse("Work in progress")
