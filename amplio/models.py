0b54baa097ed6d597d2ebdcfea6896d5b1cee43d
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-16 21:57:13 +0530

    Renamed User.type to User.post

--- amplio/models.py
+++ amplio/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password_hash = models.CharField(max_length=2047)
-    type = models.CharField(choices=choices.USER_POST_CHOICES)
+    post = models.CharField(choices=choices.USER_POST_CHOICES)
 
     def __str__(self):
         return self.name
