12f95b8818b836b2702dbb74420a496c8b4b0da8
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-16 21:32:56 +0530

    Added model for User

--- amplio/models.py
+++ amplio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from amplio import choices
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=2047)
+    type = models.CharField(choices=choices.USER_POST_CHOICES)
+
+    def __str__(self):
+        return self.name
