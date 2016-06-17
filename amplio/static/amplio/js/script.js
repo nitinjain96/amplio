573d15f6c69a807b9bc008e9f47fdb70776a7e86
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-18 04:06:24 +0530

    Added a function to toggle the visibility of specified password field

--- amplio/static/amplio/js/script.js
+++ amplio/static/amplio/js/script.js
@@ -5,3 +5,14 @@ function showSidebar() {
 function goTo(url) {
     window.location = url;
 }
+
+function toggleVisibility(button, field_identifier) {
+    var $field = $('#'+field_identifier);
+    if ($field.attr('type') === 'password') {
+        $field.attr('type', 'text');
+        button.innerHTML = '<span class="fa fa-eye-slash"></span>';
+    } else {
+        $field.attr('type', 'password');
+        button.innerHTML = '<span class="fa fa-eye"></span>';
+    }
+}
