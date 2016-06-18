44cdf4b4f5f5e8ebfcc24d8eb8c5ae0e7d8cd88f
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-19 03:25:09 +0530

    Added placeholder to sign-up form fields

--- amplio/forms.py
+++ amplio/forms.py
@@ -5,6 +5,7 @@ class SignUpForm(forms.Form):
     name = forms.CharField(
         widget=forms.TextInput(attrs={
             'id': 'up-name',
+            'placeholder': 'Name',
         }),
         label='Name',
         max_length=255
@@ -12,12 +13,14 @@ class SignUpForm(forms.Form):
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={
             'id': 'up-email',
+            'placeholder': 'Email address',
         }),
         label='Email address'
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
             'id': 'up-password',
+            'placeholder': 'Password',
         }),
         label='Password'
     )
