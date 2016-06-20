92e0cdcc8312aa49d4398851a43e8f51d7eee5af
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-20 06:27:00 +0530

    Completely revamped the sign-up and sign-in routines so that they have their own separate pages and views

--- amplio/views.py
+++ amplio/views.py
@@ -1,12 +1,16 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from hashlib import md5
 
-from amplio import forms
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from amplio import models, forms
 
 
 def index(request):
     sign_in_form = forms.SignInForm()
     sign_up_form = forms.SignUpForm()
+    print(request.session.get('user_email', '~'))
     return render(request, 'amplio/index.html', {
         'sign_in_form': sign_in_form,
         'sign_up_form': sign_up_form,
@@ -34,11 +38,51 @@ def search(request):
 
 
 def sign_in(request):
-    return HttpResponse("Work in progress")
+    if request.method == 'GET':
+        form = forms.SignInForm()
+        return render(request, 'amplio/sign-in.html', {
+            'state': 'blank',
+            'form': form,
+        })
+    if request.method == 'POST':
+        form = forms.SignInForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            request.session['user_email'] = email
+            return redirect(reverse('amplio:index'))
+        else:
+            return render(request, 'amplio/sign-in.html', {
+                'state': 'failure',
+                'form': form,
+            })
 
 
 def sign_up(request):
-    return HttpResponse("Work in progress")
+    if request.method == 'GET':
+        form = forms.SignUpForm()
+        return render(request, 'amplio/sign-up.html', {
+            'state': 'blank',
+            'form': form,
+        })
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            new_form = forms.SignUpForm()
+            name = form.cleaned_data.get('name')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            password_hash = md5(password.encode('utf-8')).hexdigest()
+            user = models.User(name=name, email=email, password_hash=password_hash, post=1)
+            user.save()
+            return render(request, 'amplio/sign-up.html', {
+                'state': 'success',
+                'form': new_form,
+            })
+        else:
+            return render(request, 'amplio/sign-up.html', {
+                'state': 'failure',
+                'form': form,
+            })
 
 
 def terms(request):
