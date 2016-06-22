89ae145f9dce8e6c05a583620976163c7791539b
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-23 01:12:34 +0530

    Removed magic numbers, added some defaults and finally split lines into chunks to conform to PEP8

--- amplio/models.py
+++ amplio/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.core.files.storage import FileSystemStorage
 from django.db import models
 
-from amplio import choices
+from amplio import choices, choice_constants
 from django_apps import settings
 
 fs = FileSystemStorage(location=settings.STATIC_URL)
@@ -13,7 +13,8 @@ class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password_hash = models.CharField(max_length=2047)
-    post = models.IntegerField(choices=choices.USER_POST_CHOICES)
+    post = models.IntegerField(choices=choices.USER_POST_CHOICES,
+                               default=choice_constants.STUDENT)
 
     def __str__(self):
         return self.name
@@ -22,16 +23,23 @@ class User(models.Model):
 class Feedback(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    type = models.IntegerField(choices=choices.FEEDBACK_TYPE_CHOICES)
-    to = models.IntegerField(choices=choices.FEEDBACK_TO_CHOICES)
-    category = models.IntegerField(choices=choices.FEEDBACK_CATEGORY_CHOICES)
-    image = models.ImageField(upload_to='amplio/images', storage=fs, blank=True)
+    type = models.IntegerField(choices=choices.FEEDBACK_TYPE_CHOICES,
+                               default=choice_constants.NOT_SURE)
+    to = models.IntegerField(choices=choices.FEEDBACK_TO_CHOICES,
+                             default=choice_constants.IMG)
+    category = models.IntegerField(choices=choices.FEEDBACK_CATEGORY_CHOICES,
+                                   default=choice_constants.NO_SPECIFIC_CATEGORY)
+    image = models.ImageField(upload_to='amplio/images',
+                              storage=fs,
+                              blank=True)
 
     time = models.DateTimeField(default=datetime.now)
     votes = models.IntegerField(default=0)
-    status = models.IntegerField(choices=choices.FEEDBACK_STATUS_CHOICES, default=0)
+    status = models.IntegerField(choices=choices.FEEDBACK_STATUS_CHOICES,
+                                 default=choice_constants.REPORTED)
 
-    by = models.ForeignKey(User, on_delete=models.CASCADE)
+    by = models.ForeignKey(User,
+                           on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -44,9 +52,13 @@ class Comment(models.Model):
     upon = models.IntegerField(choices=choices.COMMENT_UPON_CHOICES)
     votes = models.IntegerField(default=0)
 
-    upon_feedback = models.ForeignKey(Feedback, on_delete=models.CASCADE)
-    upon_comment = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
-    by = models.ForeignKey(User, on_delete=models.CASCADE)
+    upon_feedback = models.ForeignKey(Feedback,
+                                      on_delete=models.CASCADE)
+    upon_comment = models.ForeignKey('self',
+                                     on_delete=models.CASCADE,
+                                     blank=True)
+    by = models.ForeignKey(User,
+                           on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
