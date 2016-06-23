c8bfc23e5c99840822c219a5f211527a9bba76bc
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-23 20:24:35 +0530

    Added AJAX setup and a method to vote for your favourite feedback via AJAX POST

--- amplio/static/amplio/js/script.js
+++ amplio/static/amplio/js/script.js
@@ -24,3 +24,45 @@ function initializeDropdowns() {
 function initializePopups() {
     $('label').popup({inline: true});
 }
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // These HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function setUpAjax() {
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        }
+    });
+}
+
+function vote(div, feedback_id, url) {
+    $.post(
+        url,
+        {id: feedback_id},
+        function (data) {
+            $('#votes-' + feedback_id).html(data);
+            $(div).addClass("disabled");
+        }
+    )
+}
