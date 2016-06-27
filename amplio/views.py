d64d702305599309ded4d01fcde9bd39f18efa5d
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-27 19:01:28 +0530

    Added a hash of the combined string containing name and email to the session variables on sign-in

--- amplio/views.py
+++ amplio/views.py
@@ -125,6 +125,7 @@ def sign_in(request):
             user = models.User.objects.get(email=email)
             request.session['user_name'] = user.name
             request.session['user_email'] = user.email
+            request.session['user_name_email_hash'] = md5((user.name+user.email).encode('utf-8')).hexdigest()
             if user.image:
                 request.session['user_image_url'] = user.image.url
             return redirect(reverse('amplio:index'))
