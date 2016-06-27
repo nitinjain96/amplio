d7b2d102a8e4b2cbb50d40cd2a223d1894314c2c
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-27 07:24:56 +0530

    WSGI script to deploy Django over Apache

--- django_apps/wsgi.py
+++ django_apps/wsgi.py
@@ -1,16 +1,22 @@
-"""
-WSGI config for django_apps project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtual environment to work with
+site.addsitedir('/home/dhruv/Environments/python3.5env/lib/python3.5/site-packages')
+
+# Add the app directory to the PYTHONPATH
+sys.path.append('/home/dhruv/PycharmProjects/django-apps')
+sys.path.append('/home/dhruv/PycharmProjects/django-apps/django_apps')
+
+# Load settings from django_apps
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_apps.settings'
+
+# Activate your virtual environment
+activate_env = "/home/dhruv/Environments/python3.5env/bin/activate_this.py"
+exec(compile(open(activate_env).read(), activate_env, 'exec'), dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_apps.settings")
-
+# Launch the application over WSGI
 application = get_wsgi_application()
