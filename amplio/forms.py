383e7e59b1c2b0025dc16e69bf2ead7b3e91a4f9
Dhruv Bhanushali <dhruv_b@live.com>
2016-06-29 06:32:29 +0530

    Rearranged form classes and added a form to edit profile

--- amplio/forms.py
+++ amplio/forms.py
@@ -6,6 +6,144 @@ from django.core.exceptions import ValidationError
 from amplio import emails, models, choices
 
 
+class ContactForm(forms.Form):
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'id': 'contact-name',
+            'placeholder': 'Name',
+        }),
+        label='Name',
+        max_length=255,
+        required=False
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'id': 'contact-email',
+            'placeholder': 'Email address',
+        }),
+        label='Email address',
+        required=False
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'id': 'contact-message',
+        }),
+        label='Message',
+        required=False
+    )
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if len(name) == 0:
+            raise ValidationError('Please enter your name', code='missing_name')
+        return name
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if len(email) == 0:
+            raise ValidationError('Please enter your email address', code='missing_email')
+        if not emails.is_valid(email):
+            raise ValidationError('Please enter a valid email address', code='invalid_email')
+        if not emails.is_unused(email):
+            raise ValidationError('The email address is already associated with an account', code='used_email')
+        return email
+
+    def clean_message(self):
+        message = self.cleaned_data['message']
+        if len(message) == 0:
+            raise ValidationError('Message cannot be empty', code='missing_message')
+        return message
+
+
+class EditProfileForm(forms.Form):
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'id': 'edit-name',
+            'placeholder': 'Name',
+        }),
+        label='Name',
+        required=False
+    )
+    image = forms.ImageField(
+        widget=forms.ClearableFileInput(attrs={
+            'id': 'edit-image',
+            'placeholder': 'Name',
+        }),
+        label='Profile photo',
+        required=False
+    )
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if len(name) == 0:
+            raise ValidationError('Please enter your name', code='missing_name')
+        return name
+
+
+class FeedbackForm(forms.Form):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'id': 'feedback-title',
+            'placeholder': 'Title',
+        }),
+        label='Title',
+        required=False
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'id': 'feedback-description',
+        }),
+        label='Description',
+        required=False
+    )
+    type = forms.ChoiceField(
+        widget=forms.Select(attrs={
+            'id': 'feedback-type',
+            'class': 'ui dropdown',
+        }),
+        label='Type',
+        choices=choices.FEEDBACK_TYPE_CHOICES,
+        required=False
+    )
+    to = forms.ChoiceField(
+        widget=forms.Select(attrs={
+            'id': 'feedback-to',
+            'class': 'ui dropdown',
+        }),
+        label='To',
+        choices=choices.FEEDBACK_TO_CHOICES,
+        required=False
+    )
+    category = forms.ChoiceField(
+        widget=forms.Select(attrs={
+            'id': 'feedback-category',
+            'class': 'ui dropdown',
+        }),
+        label='Category',
+        choices=choices.FEEDBACK_CATEGORY_CHOICES,
+        required=False
+    )
+    image = forms.ImageField(
+        widget=forms.ClearableFileInput(attrs={
+            'id': 'feedback-image',
+        }),
+        label='Image',
+        required=False
+    )
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) == 0:
+            raise ValidationError('Please enter a title', code='missing_title')
+        return title
+
+    def clean_description(self):
+        description = self.cleaned_data['description']
+        if len(description) == 0:
+            raise ValidationError('Please enter a description', code='missing_description')
+        return description
+
+
 class SignInForm(forms.Form):
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={
@@ -101,116 +239,3 @@ class SignUpForm(forms.Form):
         if 0 < len(password) < 8:
             raise ValidationError('Your password should be at least 8 characters long', code='short_password')
         return password
-
-
-class ContactForm(forms.Form):
-    name = forms.CharField(
-        widget=forms.TextInput(attrs={
-            'id': 'contact-name',
-            'placeholder': 'Name',
-        }),
-        label='Name',
-        max_length=255,
-        required=False
-    )
-    email = forms.EmailField(
-        widget=forms.EmailInput(attrs={
-            'id': 'contact-email',
-            'placeholder': 'Email address',
-        }),
-        label='Email address',
-        required=False
-    )
-    message = forms.CharField(
-        widget=forms.Textarea(attrs={
-            'id': 'contact-message',
-        }),
-        label='Message',
-        required=False
-    )
-
-    def clean_name(self):
-        name = self.cleaned_data['name']
-        if len(name) == 0:
-            raise ValidationError('Please enter your name', code='missing_name')
-        return name
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if len(email) == 0:
-            raise ValidationError('Please enter your email address', code='missing_email')
-        if not emails.is_valid(email):
-            raise ValidationError('Please enter a valid email address', code='invalid_email')
-        if not emails.is_unused(email):
-            raise ValidationError('The email address is already associated with an account', code='used_email')
-        return email
-
-    def clean_message(self):
-        message = self.cleaned_data['message']
-        if len(message) == 0:
-            raise ValidationError('Message cannot be empty', code='missing_message')
-        return message
-
-
-class FeedbackForm(forms.Form):
-    title = forms.CharField(
-        widget=forms.TextInput(attrs={
-            'id': 'feedback-title',
-            'placeholder': 'Title'
-        }),
-        label='Title',
-        required=False
-    )
-    description = forms.CharField(
-        widget=forms.Textarea(attrs={
-            'id': 'feedback-description'
-        }),
-        label='Description',
-        required=False
-    )
-    type = forms.ChoiceField(
-        widget=forms.Select(attrs={
-            'id': 'feedback-type',
-            'class': 'ui dropdown',
-        }),
-        label='Type',
-        choices=choices.FEEDBACK_TYPE_CHOICES,
-        required=False
-    )
-    to = forms.ChoiceField(
-        widget=forms.Select(attrs={
-            'id': 'feedback-to',
-            'class': 'ui dropdown',
-        }),
-        label='To',
-        choices=choices.FEEDBACK_TO_CHOICES,
-        required=False
-    )
-    category = forms.ChoiceField(
-        widget=forms.Select(attrs={
-            'id': 'feedback-category',
-            'class': 'ui dropdown',
-        }),
-        label='Category',
-        choices=choices.FEEDBACK_CATEGORY_CHOICES,
-        required=False
-    )
-    image = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={
-            'id': 'feedback-image',
-        }),
-        label='Image',
-        required=False
-    )
-
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if len(title) == 0:
-            raise ValidationError('Please enter a title', code='missing_title')
-        return title
-
-    def clean_description(self):
-        description = self.cleaned_data['description']
-        if len(description) == 0:
-            raise ValidationError('Please enter a description', code='missing_description')
-        return description
