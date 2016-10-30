5b6ffcab7d75d6c2caec55c995c07e3b0310a169
Dhruv Bhanushali <dhruv_b@live.com>
2016-10-30 15:05:07 +0530

    Code formatting to match the style guide

--- amplio/admin.py
+++ amplio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,Feedback,Comment
+from amplio.models import User, Feedback, Comment
 # Register your models here.
 
 admin.site.register(User)
