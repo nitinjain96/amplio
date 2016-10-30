c56c0090a4307045dbcf603d0a44b2462451ad48
Pranjal Tale <pranjaltale16@gmail.com>
2016-10-30 14:04:27 +0530

    Registered models in admin.py

--- amplio/admin.py
+++ amplio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User,Feedback,Comment
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Feedback)
+admin.site.register(Comment)
